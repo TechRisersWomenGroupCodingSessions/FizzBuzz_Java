cf9da1f0817ebafa1a2b79a96bae596b9e81d95b
I337592 <parisanoushazaran@gmail.com>
2024-03-27 18:54:39 +0000

    all tests failed

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,19 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
 
-    public String FizzBuzzMethod(int a) {
-        if(a%3 == 0 && a%5 == 0) {
-            return "FizzBuzz";
+    public List<String> FizzBuzzMethod(List<Integer> listOfNumbers) {
+        List<String> listAsString = new ArrayList<>();
+        for (int i = 0; i < listOfNumbers.size(); i++) {
+            if(listOfNumbers.get(i)%3 == 0 && listOfNumbers.get(i)%5 == 0) {
+                listAsString.add("FizzBuzz");
+            }
+            else if(listOfNumbers.get(i)%3 == 0) {
+                listAsString.add("Fizz");
+            }
+            else if(listOfNumbers.get(i)%5 == 0) {
+                listAsString.add("Buzz");
+            }
+            else {
+                listAsString.add(listOfNumbers.get(i) + "");
+            }
         }
-        else if(a%3 == 0) {
-            return "Fizz";
-        }
-        else if(a%5 == 0) {
-            return "Buzz";
-        }
-        else {
-            return a + "";
-        }
+        return listAsString;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,6 +1,7 @@
 import org.example.FizzBuzz;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,35 +10,54 @@ public class FizzBuzzTest {
 
 @Test void represent1AsAString() {
     FizzBuzz fizzbuzz = new FizzBuzz();
-    assertEquals(fizzbuzz.FizzBuzzMethod(1),"1");
+    List<Integer> listOfNumbers = List.of(1);
+    List<String> listOfStrings = List.of("1");
+    assertEquals(fizzbuzz.FizzBuzzMethod(listOfNumbers),listOfStrings);
 }
 
 @Test void represent3AsFizz() {
     FizzBuzz fizzbuzz = new FizzBuzz();
-    assertEquals(fizzbuzz.FizzBuzzMethod(3),"Fizz");
+    List<Integer> listOfNumbers = List.of(3);
+    List<String> listOfStrings = List.of("Fizz");
+    assertEquals(fizzbuzz.FizzBuzzMethod(listOfNumbers),listOfStrings);
 }
 
     @Test void divisibleBy3isFizz() {
         FizzBuzz fizzbuzz = new FizzBuzz();
-        assertEquals(fizzbuzz.FizzBuzzMethod(9),"Fizz");
+        List<Integer> listOfNumbers = List.of(9);
+        List<String> listOfStrings = List.of("Fizz");
+        assertEquals(fizzbuzz.FizzBuzzMethod(listOfNumbers),listOfStrings);
     }
+
 
     @Test void divisibleby5isBuzz() {
         FizzBuzz fizzbuzz = new FizzBuzz();
-        assertEquals(fizzbuzz.FizzBuzzMethod(10),"Buzz");
+        List<Integer> listOfNumbers = List.of(10);
+        List<String> listOfStrings = List.of("Buzz");
+        assertEquals(fizzbuzz.FizzBuzzMethod(listOfNumbers),listOfStrings);
     }
+
 
     @Test void divisibleby5and3isFizzBuzz() {
         FizzBuzz fizzbuzz = new FizzBuzz();
-        assertEquals(fizzbuzz.FizzBuzzMethod(15),"FizzBuzz");
+        List<Integer> listOfNumbers = List.of(15);
+        List<String> listOfStrings = List.of("FizzBuzz");
+        assertEquals(fizzbuzz.FizzBuzzMethod(listOfNumbers),listOfStrings);
     }
 
     @Test void notdivisibleby5or3isStringRepresentativeOfNumber() {
         FizzBuzz fizzbuzz = new FizzBuzz();
-        assertEquals(fizzbuzz.FizzBuzzMethod(13),"13");
+        List<Integer> listOfNumbers = List.of(13);
+        List<String> listOfStrings = List.of("13");
+        assertEquals(fizzbuzz.FizzBuzzMethod(listOfNumbers),listOfStrings);
     }
 
-    //list of numbers
+    @Test void takesAListOfNummbers() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        List<Integer> listOfNumbers = List.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+        List<String> listOfStrings = List.of("1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz");
+        assertEquals(fizzbuzz.FizzBuzzMethod(listOfNumbers), listOfStrings);
+    }
 
 
 }
