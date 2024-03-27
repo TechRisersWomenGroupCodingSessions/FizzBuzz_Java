42076f5132fd98fe87273e3b1af48ed1076fc29a
I337592 <parisanoushazaran@gmail.com>
2024-03-27 18:31:29 +0000

    three tests passed

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -3,7 +3,11 @@ package org.example;
 public class FizzBuzz {
 
     public String FizzBuzzMethod(int a) {
-        return a + "";
+        if(a%3 == 0) {
+            return "Fizz";
+        }
+        else {
+            return a + "";
+        }
     }
-
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,13 +1,26 @@
 import org.example.FizzBuzz;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
-@Test String represent1AsAString() {
+
+@Test void represent1AsAString() {
     FizzBuzz fizzbuzz = new FizzBuzz();
-    String oneAsAString = fizzbuzz.FizzBuzzMethod(1);
-    return oneAsAString;
+    assertEquals(fizzbuzz.FizzBuzzMethod(1),"1");
 }
+
+@Test void represent3AsFizz() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    assertEquals(fizzbuzz.FizzBuzzMethod(3),"Fizz");
+}
+
+    @Test void divisibleBy3isFizz() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        assertEquals(fizzbuzz.FizzBuzzMethod(15),"Fizz");
+    }
 
 
 }
